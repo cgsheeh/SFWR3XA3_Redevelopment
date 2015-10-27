628d65a15b5c51cb7d4a68e1e6babc01a712a538
Connor Sheehan <sheehacg@mcmaster.ca>
2015-10-27 16:30:05 -0400

    Add initial file for GUI

--- src/redevbazaar.py
+++ src/redevbazaar.py
@@ -8,5 +8,40 @@ class WelcomeScreen(BaseWidget):
 		self.testText = ControlText("WHERE IS THIS") 	
 		
 		
+		self.mainmenu = [
+			{ 'Home': [
+					{'My Listings': self.__getlistingsEvent},
+					'-',
+					{'My Orders': self.__getordersEvent},
+					{'Settings': self.__getsettingsEvent}
+				]
+			},
+			{ 'Messages': [
+					{'Inbox': self.__getinboxEvent},
+					{'Send a message': self.__sendmessageEvent},
+					{'Sent Messages': self.__getoutboxEvent}
+				]
+			}
+	    ]
+		
+	def __getlistingsEvent(self):
+		print "hey"
+		
+	def __getordersEvent(self):
+		print "hey"
+		
+	def __getsettingsEvent(self):
+		print "hey"
+		
+	def __getinboxEvent(self):
+		print "hey"
+		
+	def __sendmessageEvent(self):
+		print "hey"
+		
+	def __getoutboxEvent(self):
+		print "hey"
+		
+		
 if __name__ == "__main__":
 	pyforms.startApp(WelcomeScreen)
