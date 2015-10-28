1a03919d8c5eeb7cb8a8fe835c7724a8c1b818fe
Connor Sheehan <sheehacg@mcmaster.ca>
2015-10-28 15:46:36 -0400

    Refactor code to comply with PEP8 conventions.

--- src/redevbazaar.py
+++ src/redevbazaar.py
@@ -2,102 +2,103 @@ import pyforms
 from pyforms import BaseWidget
 from pyforms.Controls import ControlText
 
+
 class WelcomeScreen(BaseWidget):
-	def __init__(self):
-		super(WelcomeScreen, self).__init__("TEST1")
-		self.testText = ControlText("some stuff") 	
-		self.name = 'TestUsername'
-		
-		self.mainmenu = [
-			{ self.name : [
-					{'My Listings': self.__getlistingsEvent},
-					{'My Orders': self.__getordersEvent},
-					{'Settings': self.__getsettingsEvent}
-				]
-			},
-			
-			{ 'Messages': [
-					{'Compose': self.__getinboxEvent},
-					{'Inbox': self.__sendmessageEvent},
-					{'Sent': self.__getoutboxEvent}
-				]
-			},
-			
-			{
-				'Contracts' : [
-					{'Purchased' : self.__purchasedcontractsEvent},
-					{'Sold' : self.__soldcontractsEvent},
-					{'Active': self.__activecontractsEvent}
-				]
-			},
-			
-			{
-				'Stores' : [
-					{'Create new store': self.__createstoreEvent},
-					{'View stores' : self.__viewstoreEvent},
-					{'Find stores' : self.__searchstoreEvent}
-				]
-			},
-			
-			{
-				'Notaries' : [
-					{'Become a notary' : self.__becomenotaryEvent},
-					{'Find notaries': self.__findnotaryEvent},
-					{'Notary help' : self.__helpnotaryEvent}
-				]
-			},
-			
-			{'Search' : [{'Search':self.__searchEvent}]}
-	    ]
-		
-	def __getlistingsEvent(self):
-		print "hey"
-		
-	def __getordersEvent(self):
-		print "hey"
-		
-	def __getsettingsEvent(self):
-		print "hey"
-		
-	def __getinboxEvent(self):
-		print "hey"
-		
-	def __sendmessageEvent(self):
-		print "hey"
-		
-	def __getoutboxEvent(self):
-		print "hey"
-		
-	def __activecontractsEvent(self):
-		print "hey"
-		
-	def __soldcontractsEvent(self):
-		print "hey"
-		
-	def __purchasedcontractsEvent(self):
-		print "hey"
-		
-	def __createstoreEvent(self):
-		print "hey"
-		
-	def __viewstoreEvent(self):
-		print "view store"
-		
-	def __searchstoreEvent(self):
-		print "search store"
-		
-	def __becomenotaryEvent(self):
-		print "become a notary"
-		
-	def __findnotaryEvent(self):
-		print "find a notary"
-		
-	def __helpnotaryEvent(self):
-		print "help a notary"
-		
-	def __searchEvent(self):
-		print "Search event"
-		
-		
+    def __init__(self):
+        super(WelcomeScreen, self).__init__("TEST1")
+        self.testText = ControlText("some stuff")
+        self.name = 'TestUsername'
+
+        self.mainmenu = [
+            {self.name: [
+                {'My Listings': self.__getlistingsevent},
+                {'My Orders': self.__getordersevent},
+                {'Settings': self.__getsettingsevent}
+            ]
+            },
+
+            {'Messages': [
+                {'Compose': self.__getinboxevent},
+                {'Inbox': self.__sendmessageevent},
+                {'Sent': self.__getoutboxevent}
+            ]
+            },
+
+            {
+                'Contracts': [
+                    {'Purchased': self.__purchasedcontractsevent},
+                    {'Sold': self.__soldcontractsevent},
+                    {'Active': self.__activecontractsevent}
+                ]
+            },
+
+            {
+                'Stores': [
+                    {'Create new store': self.__createstoreevent},
+                    {'View stores': self.__viewstoreevent},
+                    {'Find stores': self.__searchstoreevent}
+                ]
+            },
+
+            {
+                'Notaries': [
+                    {'Become a notary': self.__becomenotaryevent},
+                    {'Find notaries': self.__findnotaryevent},
+                    {'Notary help': self.__helpnotaryevent}
+                ]
+            },
+
+            {'Search': [{'Search': self.__searchevent}]}
+        ]
+
+    def __getlistingsevent(self):
+        print "hey"
+
+    def __getordersevent(self):
+        print "hey"
+
+    def __getsettingsevent(self):
+        print "hey"
+
+    def __getinboxevent(self):
+        print "hey"
+
+    def __sendmessageevent(self):
+        print "hey"
+
+    def __getoutboxevent(self):
+        print "hey"
+
+    def __activecontractsevent(self):
+        print "hey"
+
+    def __soldcontractsevent(self):
+        print "hey"
+
+    def __purchasedcontractsevent(self):
+        print "hey"
+
+    def __createstoreevent(self):
+        print "hey"
+
+    def __viewstoreevent(self):
+        print "view store"
+
+    def __searchstoreevent(self):
+        print "search store"
+
+    def __becomenotaryevent(self):
+        print "become a notary"
+
+    def __findnotaryevent(self):
+        print "find a notary"
+
+    def __helpnotaryevent(self):
+        print "help a notary"
+
+    def __searchevent(self):
+        print "Search event"
+
+
 if __name__ == "__main__":
-	pyforms.startApp(WelcomeScreen)
+    pyforms.startApp(WelcomeScreen)
